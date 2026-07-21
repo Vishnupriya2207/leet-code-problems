9413b57907d00e26a9c0699844389db6b80dbfff
Vishnupriya2207 <v73286328@gmail.com>
2026-07-21 22:01:19 +0530

    Create prob-1832 check if the sentance  is pangram.java

---
+++ prob-1832 check if the sentance  is pangram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        if (sentence.length() < 26) {
+            return false;
+        }
+
+        boolean[] seen = new boolean[26];
+
+        for (char ch : sentence.toCharArray()) {
+            seen[ch - 'a'] = true;
+        }
+
+        for (boolean letter : seen) {
+            if (!letter) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
